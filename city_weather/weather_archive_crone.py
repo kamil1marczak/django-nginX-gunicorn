5b9a7f669dd5c4f3bdfd528f735c3794c2995638
Kamil Marczak <kamil1marczak@gmail.com>
2020-01-11 19:29:14 +0100

    stanalone script for archive

--- city_weather/weather_archive_crone.py
+++ city_weather/weather_archive_crone.py
@@ -2,49 +2,21 @@ import os
 import django
 from urllib import request
 
-
-os.environ['DJANGO_SETTINGS_MODULE'] = "flight_analyser.settings"
+os.environ['DJANGO_SETTINGS_MODULE'] = "city_weather.settings"
 django.setup()
 
-from acb.flights_archive import FlightArchive
+from weather_app.weather_archive import WeatherArchiveCreator
 
-
-delay_selector = '{"status":"active","isDelay":"delay"}'
-canceled_selector = '{"status":"canceled","isDelay":"notDelay"}'
-
-target_data = [{'airport_selector': 'WAW', 'type_selector': 'departure', 'canceled_deleyed_selector': delay_selector},
-               {'airport_selector': 'WAW', 'type_selector': 'departure', 'canceled_deleyed_selector': canceled_selector},
-               {'airport_selector': 'WMI', 'type_selector': 'departure', 'canceled_deleyed_selector': delay_selector},
-               {'airport_selector': 'WMI', 'type_selector': 'departure', 'canceled_deleyed_selector': canceled_selector},
-               {'airport_selector': 'KRK', 'type_selector': 'departure', 'canceled_deleyed_selector': delay_selector},
-               {'airport_selector': 'KRK', 'type_selector': 'departure', 'canceled_deleyed_selector': canceled_selector},
-               {'airport_selector': 'KTW', 'type_selector': 'departure', 'canceled_deleyed_selector': delay_selector},
-               {'airport_selector': 'KTW', 'type_selector': 'departure', 'canceled_deleyed_selector': canceled_selector},
-               {'airport_selector': 'GDN', 'type_selector': 'departure', 'canceled_deleyed_selector': delay_selector},
-               {'airport_selector': 'GDN', 'type_selector': 'departure', 'canceled_deleyed_selector': canceled_selector},
-               {'airport_selector': 'WRO', 'type_selector': 'departure', 'canceled_deleyed_selector': delay_selector},
-               {'airport_selector': 'WRO', 'type_selector': 'departure', 'canceled_deleyed_selector': canceled_selector},
-               {'airport_selector': 'POZ', 'type_selector': 'departure', 'canceled_deleyed_selector': delay_selector},
-               {'airport_selector': 'POZ', 'type_selector': 'departure', 'canceled_deleyed_selector': canceled_selector},
-               {'airport_selector': 'BZG', 'type_selector': 'departure', 'canceled_deleyed_selector': delay_selector},
-               {'airport_selector': 'BZG', 'type_selector': 'departure', 'canceled_deleyed_selector': canceled_selector},
+target_data = [{'city': 'Warsaw'},
+               {'city': 'Poznań'},
+               {'city': 'Wrocław'},
 
                ]
 
-
 for i in target_data:
-    try:
-        FlightArchive.save_to_flight_archive(i['airport_selector'], i['type_selector'], i['canceled_deleyed_selector'])
-    except:
-        pass
+    WeatherArchiveCreator.save_to_weather_archive(i['city'])
 
 
-# type = ['departure', 'arrival' ]
-# status = ['active', 'canceled']
-# isDelay = ['delay', 'notDelay']
-# airport = [iata code]
-# '{"status":"active","isDelay":"delay"}'
-# '{"status":"canceled","isDelay":"notDelay"}'
 
 
 # bash to set up script every 30 minutes:
