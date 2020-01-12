98555dbe6940ec746aa02928015307f3306c3bf2
Kamil Marczak <kamil1marczak@gmail.com>
2020-01-12 15:07:29 +0100

    Dockerfile

--- python_for_cron.sh
+++ python_for_cron.sh
@@ -3,7 +3,7 @@
 docker build -t cron_image_entrypoint/develop .
 docker stop prod_container_cron || true 
 
-docker run --env-file ./.env  --rm -ti -d -p 40:8040 --name prod_container_cron cron_image_entrypoint/develop
+docker run --env-file .env  --rm -ti -d -p 40:8040 --name prod_container_cron cron_image_entrypoint/develop
 docker exec -it prod_container_cron bash -c "python3 /src/weather_archive_crone.py"
 
 
